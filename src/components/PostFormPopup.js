0fcd3f4f7f7c623d2d909b06673452004c9fe9f0
Ishan Bankhele <bankheleishan@gmail.com>
2023-11-18 22:58:03 +0530

    Fixed create community bug

--- src/components/PostFormPopup.js
+++ src/components/PostFormPopup.js
@@ -83,7 +83,7 @@ function PostFormPopup(){
             className={"row position-fixed position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center z-2 "+visibleClass} 
             style={{backgroundColor: 'rgba(0,0,0,0.8)'}} //faded background 
         >   
-            <form className="col-lg-4 col-md-6 col-10 text-light bgBlack border border-2 border-danger mx-auto p-4 rounded-2">
+            <div className="col-lg-4 col-md-6 col-10 text-light bgBlack border border-2 border-danger mx-auto p-4 rounded-2">
                 
                 <h4 className="text-light mb-4">{topText}</h4>
                 <input 
@@ -121,7 +121,7 @@ function PostFormPopup(){
                     </button>
                 </div>
                 
-            </form>
+            </div>
         </div>
     )
 }
